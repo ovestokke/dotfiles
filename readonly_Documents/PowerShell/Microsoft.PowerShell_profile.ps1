3e26680c0d16e92694de099ab5b7904347c225dc
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2025-10-16 20:20:28 +0200

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,2 @@
 oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/catppuccin_mocha.omp.json | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
