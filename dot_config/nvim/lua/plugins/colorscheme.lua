c7ced48bb0c5fa8381ae6aad7b23757ff3dbac22
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2025-10-16 13:53:10 +0200

    Add .config/nvim/lua/plugins/colorscheme.lua Update .config/nvim/lua/plugins/example.lua

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = false,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mason = true,
+        which_key = true,
+        telescope = {
+          enabled = true,
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
