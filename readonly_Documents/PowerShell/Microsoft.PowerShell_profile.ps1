e1b98cc5cd87f5dc6add2075070048146f5049a7
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2025-10-17 09:59:09 +0200

    Remove Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,2 +0,0 @@
-oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/catppuccin_mocha.omp.json | Invoke-Expression
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
