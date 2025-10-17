21f51869bdfd8ced35c8d2eaa625cffcfdcbc30e
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2025-10-17 16:29:51 +0200

    update: 2025-10-17

--- .chezmoitemplates/powershell/profile.ps1
+++ .chezmoitemplates/powershell/profile.ps1
@@ -1,2 +1,11 @@
 oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/catppuccin_mocha.omp.json | Invoke-Expression
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+function dotsync {
+    Push-Location "$env:USERPROFILE\.local\share\chezmoi"
+    git add -A
+    git commit -m "update: $(Get-Date -Format yyyy-MM-dd)"
+    git pull --rebase
+    git push
+    Pop-Location
+}
