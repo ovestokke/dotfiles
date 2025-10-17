c5052626cf69d8599001c2f3fd7db8466dc00c70
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2025-10-17 16:33:55 +0200

    Update profile.ps1

--- .chezmoitemplates/powershell/profile.ps1
+++ .chezmoitemplates/powershell/profile.ps1
@@ -1,6 +1,8 @@
 oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/catppuccin_mocha.omp.json | Invoke-Expression
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+Set-Alias -Name ch -Value chezmoi
+
 function dotsync {
     Push-Location "$env:USERPROFILE\.local\share\chezmoi"
     git add -A
