3221e1e3160a60587b63a04dbd78668002a93b73
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2026-06-25 12:01:03 +0200

    modify fish scripts

--- home/dot_config/fish/functions/ff.fish
+++ home/dot_config/fish/functions/ff.fish
@@ -1,3 +1,66 @@
-function ff
-    $EDITOR (fd --type f | fzf --preview 'bat --color=always --style=numbers {}')
+function ff -d "Find files with fd + fzf and open the selected file"
+    set -l finder fd
+    if not type -q fd
+        if type -q fdfind
+            set finder fdfind
+        else if type -q find
+            set finder find
+        else
+            echo "ff: fd/fdfind/find is not installed"
+            return 1
+        end
+    end
+
+    set -l bat_cmd bat
+    if not type -q bat
+        if type -q batcat
+            set bat_cmd batcat
+        else
+            set bat_cmd cat
+        end
+    end
+
+    set -l preview "$bat_cmd --color=always --style=numbers --line-range :300 {}"
+    if test "$bat_cmd" = cat
+        set preview "sed -n '1,200p' {}"
+    end
+
+    set -l file
+    if test "$finder" = find
+        set file (
+            find . -type f \
+                -not -path './.git/*' \
+                -not -path './Library/*' \
+                2>/dev/null | \
+            fzf --prompt 'files> ' \
+                --height 80% \
+                --layout reverse \
+                --border \
+                --info inline \
+                --preview "$preview" \
+                --preview-window 'right:60%'
+        )
+    else
+        set file (
+            $finder --type f \
+                --hidden \
+                --follow \
+                --exclude .git \
+                --exclude Library \
+                2>/dev/null | \
+            fzf --prompt 'files> ' \
+                --height 80% \
+                --layout reverse \
+                --border \
+                --info inline \
+                --preview "$preview" \
+                --preview-window 'right:60%'
+        )
+    end
+    test -z "$file"; and return
+
+    set -l editor $EDITOR
+    test -z "$editor"; and set editor nvim
+
+    command $editor -- $file
 end
--- home/dot_config/fish/functions/frg.fish
+++ home/dot_config/fish/functions/frg.fish
@@ -1,10 +1,51 @@
-function frg
-    set -l query $argv
-    set -l result (rg -n "$query" | fzf --delimiter : --preview 'bat --color=always --style=numbers --highlight-line {2} {1}')
+function frg -d "Search file contents with rg + fzf and open the selected match"
+    if not type -q rg
+        echo "frg: rg is not installed"
+        return 1
+    end
+
+    set -l query (string join ' ' -- $argv)
+    if test -z "$query"
+        echo "usage: frg <search text>"
+        return 1
+    end
+
+    set -l bat_cmd bat
+    if not type -q bat
+        if type -q batcat
+            set bat_cmd batcat
+        else
+            set bat_cmd cat
+        end
+    end
+
+    set -l preview "$bat_cmd --color=always --style=numbers --highlight-line {2} {1}"
+    if test "$bat_cmd" = cat
+        set preview "sed -n '1,200p' {1}"
+    end
+
+    set -l result (
+        rg --line-number --column --no-heading --smart-case \
+            --glob '!.git/**' \
+            --glob '!Library/**' \
+            -- "$query" 2>/dev/null | \
+        fzf --delimiter : \
+            --prompt 'rg> ' \
+            --height 80% \
+            --layout reverse \
+            --border \
+            --info inline \
+            --preview "$preview" \
+            --preview-window 'right:60%:+{2}-/2'
+    )
     test -z "$result"; and return
 
-    set -l file (string split -f1 : $result)
-    set -l line (string split -f2 : $result)
+    set -l parts (string split -m 3 : -- $result)
+    set -l file $parts[1]
+    set -l line $parts[2]
 
-    $EDITOR +$line $file
+    set -l editor $EDITOR
+    test -z "$editor"; and set editor nvim
+
+    command $editor +$line -- $file
 end
